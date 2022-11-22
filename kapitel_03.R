f67085c1d88e5af92fdfd4bd181a1fac650f10b2
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-11-22 06:50:51 +0100

    mit kapitel 3 begonnen

--- kapitel_03.R
+++ kapitel_03.R
@@ -1,5 +1,60 @@
 library(readr)
 
+
+# 3.1.1 Kartenstapel mit Vektor verwalten
+
+stapel <- c(4, 7, 6, 2, 3, 5)
+stapel
+
+
+# 3.1.2 Anzahl der Karten zaehlen
+length(stapel)
+
+
+# 3.1.3 Sequenzen mit Schrittweite 1
+
+# Integers
+1:6
+
+# Floats
+0.25:6
+
+# absteigend
+3:-3
+
+
+# 3.2.1 Subsetting
+
+# einzelnes Element
+stapel[3]
+
+# mehrere Elemente
+stapel[c(3, 5)]
+
+# Sequenz mehrerer Elemente
+stapel[3:5]
+
+# Verschachtelter Zugriff
+# erstes Element des Subsets 3:5
+stapel[3:5][1]
+
+# Zugriff auf nicht existierende Elemente liefert NA
+stapel[c(2, 9, 3)]
+
+# Elemente ausschliessen
+stapel
+# alle ausser Positionen 3 bis 5
+stapel[-(3:5)]
+
+
+
+
+
+
+
+
+
+# Uebungen
 df <- read_csv("Datensaetze/Massen.csv")
 
 # bool Vektor
